1733a8e67d497e5f4ed349789c29a2290d9fd080
Luli <lucilapastore@gmail.com>
2023-12-23 18:35:13 -0300

    feat: add Header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import icon from "../assets/investment-calculator-logo.png";
+export default function Header() {
+  return (
+    <header id="header">
+      <img src={icon} alt="Image showing a money bag" />
+      <h1>Investment Calculator</h1>
+    </header>
+  );
+}
