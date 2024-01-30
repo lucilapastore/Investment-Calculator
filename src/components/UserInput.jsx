f463bd5d2ae4d96686c952573a561840fcd08986
Luli <lucilapastore@gmail.com>
2024-01-30 14:20:09 -0300

    feat: Handle change and use two way binding

--- src/components/UserInput.jsx
+++ src/components/UserInput.jsx
@@ -1,10 +1,34 @@
+import { useState } from "react";
 export default function UserInput() {
+  const [userInput, setUserInput] = useState({
+    initialInvestment: 10000,
+    annualInvestment: 1200,
+    expectedReturn: 6,
+    duration: 10,
+  });
+
+  function handleChange(inputIdentifier, newValue) {
+    setUserInput((prevUserInput) => {
+      return {
+        ...prevUserInput,
+        [inputIdentifier]: newValue,
+      };
+    });
+  }
+
   return (
     <section id="user-input">
       <div className="input-group">
         <p>
           <label>Initial Investment</label>
-          <input type="number" required />
+          <input
+            type="number"
+            required
+            value={userInput.initialInvestment}
+            onChange={(event) =>
+              handleChange("initialInvestment", event.target.value)
+            }
+          />
 
           <p>
             <label>Annual Investment</label>
