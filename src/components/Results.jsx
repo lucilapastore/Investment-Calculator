0e7cc094cae8aa6b802b8965d6721576772d5ebb
Luli <lucilapastore@gmail.com>
2024-04-15 23:54:16 -0300

    Feat: output results in a list

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -1,6 +1,32 @@
-import { calculateInvestmentResults } from "../util/investment.js";
+import { calculateInvestmentResults, formatter } from "../util/investment.js";
+
 export default function Results({ input }) {
   const resultsData = calculateInvestmentResults(input);
 
-  return <table id="result"></table>;
+  return (
+    <table id="result">
+      <thead>
+        <tr>
+          <th>Year</th>
+          <th>Investment Value</th>
+          <th>Interest (Year)</th>
+          <th>Total Interest</th>
+          <th>Invested Capital</th>
+        </tr>
+      </thead>
+      <tbody>
+        {resultsData.map((yearData) => {
+          return (
+            <tr key={yearData.year}>
+              <td>{yearData.year}</td>
+              <td>{formatter.format(yearData.valueEndOfYear)}</td>
+              <td>{formatter.format(yearData.interest)}</td>
+              <td></td>
+              <td></td>
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
 }
